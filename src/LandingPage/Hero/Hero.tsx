927af89d5b9921778d2967827b27791e407b0da8
Md Main Uddin <2021md.main@gmail.com>
2023-12-22 17:48:19 +0600

    hero section impl

---
+++ src/LandingPage/Hero/Hero.tsx
@@ -0,0 +1,46 @@
+import Styles from "./Hero.module.css";
+import react_logo from "@/assets/react_logo.png";
+import ts_logo from "@/assets/js_logo.png";
+import python_logo from "@/assets/python_logo.png";
+import php_logo from "@/assets/php_logo.png";
+import tailwind_logo from "@/assets/tailwind_logo.png";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.slogan_container}>
+        <h1 className={Styles.slogan}>
+          Become an Interview cracker. Give test on you stack and become a Ninja
+        </h1>
+        <button className={Styles.start_btn}>Start a Test</button>
+      </div>
+      <Image
+        src={react_logo}
+        alt="react_logo"
+        width={300}
+        style={{
+          position: "absolute",
+          top: "-50px",
+          left: "-50px",
+          zIndex: "1",
+          opacity: ".7",
+        }}
+      />
+      <Image
+        src={python_logo}
+        alt="python_logo"
+        width={300}
+        style={{
+          position: "absolute",
+          bottom: "-50px",
+          right: "-50px",
+          zIndex: "1",
+          opacity: ".7",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/LandingPage/Header/Header";
+import Hero from "@/LandingPage/Hero/Hero";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 }
